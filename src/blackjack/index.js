cd6ea9d115bd2c2e53e4a558e268eaefa2f085b5
Stefan <trfstephan@gmail.com>
2024-01-06 14:11:45 +0100

    Refactorizar el turno de la computadora

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -1,5 +1,5 @@
 import _ from 'underscore'
-import { crearDeck, pedirCarta, valorCarta } from "./usecases";
+import { crearDeck, pedirCarta, valorCarta, turnoComputadora } from "./usecases";
 
 /**
  * 2C = Two of Clubs
@@ -29,39 +29,7 @@ const puntosHTML = document.querySelectorAll('small');
 deck = crearDeck(tipos, especiales);
 console.log(deck)
 
-// turno de la computadora
-const turnoComputadora = ( puntosMinimos ) => {
 
-    do {
-        const carta = pedirCarta( deck );
-
-        puntosComputadora = puntosComputadora + valorCarta( carta );
-        puntosHTML[1].innerText = puntosComputadora;
-        
-        // <img class="carta" src="assets/cartas/2C.png">
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
-        imgCarta.classList.add('carta');
-        divCartasComputadora.append( imgCarta );
-
-        if( puntosMinimos > 21 ) {
-            break;
-        }
-
-    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
-
-    setTimeout(() => {
-        if( puntosComputadora === puntosMinimos ) {
-            alert('Nadie gana :(');
-        } else if ( puntosMinimos > 21 ) {
-            alert('Computadora gana')
-        } else if( puntosComputadora > 21 ) {
-            alert('Jugador Gana');
-        } else {
-            alert('Computadora Gana')
-        }
-    }, 100 );
-}
 
 // Eventos
 btnPedir.addEventListener('click', () => {
@@ -81,13 +49,13 @@ btnPedir.addEventListener('click', () => {
         console.warn('Lo siento mucho, perdiste');
         btnPedir.disabled   = true;
         btnDetener.disabled = true;
-        turnoComputadora( puntosJugador );
+        turnoComputadora( puntosJugador, puntosHTML[1], divCartasComputadora, deck );
 
     } else if ( puntosJugador === 21 ) {
         console.warn('21, genial!');
         btnPedir.disabled   = true;
         btnDetener.disabled = true;
-        turnoComputadora( puntosJugador );
+        turnoComputadora( puntosJugador, puntosHTML[1], divCartasComputadora, deck );
     }
 
 });
@@ -97,7 +65,7 @@ btnDetener.addEventListener('click', () => {
     btnPedir.disabled   = true;
     btnDetener.disabled = true;
 
-    turnoComputadora( puntosJugador );
+    turnoComputadora( puntosJugador, puntosHTML[1], divCartasComputadora, deck );
 });
 
 btnNuevo.addEventListener('click', () => {
--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,4 @@
 export { crearDeck } from './crear-deck'
 export { pedirCarta } from './pedir-carta'
+export { turnoComputadora } from './turno-computadora'
 export { valorCarta } from './valor-carta'
